2011686ff334b8adb4a9f71b7cda543713064aaf
Wonders123 <singsandwonder1@gmail.com>
2024-06-06 17:16:01 -0500

    my second push

---
+++ 44.perCal&GrabbingElemWitJa.js
@@ -0,0 +1,8 @@
+var numField1 = document.getElementById('numField1');
+numField1.value = "hey miracle";
+var numField2 = document.getElementById("numField2");
+numField2.value = "God's faithfulness";
+
+var resultField = document.getElementById("resultField");
+
+resultField.innerText = "TEST2";
